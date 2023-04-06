1537f2f33ef585c34d78d80e09fe2ae6ca62c811
William Filgueira dos Santos <extern.william.santos@volkswagen.com.br>
2023-04-06 15:29:14 -0300

    update

--- G4Bebidas/src/app/models/product.ts
+++ G4Bebidas/src/app/models/product.ts
@@ -1,12 +1,12 @@
 import { Category } from './category';
 
 export class Product {
-  public productId: number;
-  public name: string;
-  public description: string;
-  public price: number;
-  public category: Category;
-  public image: String;
-  public active: boolean = true;
+  productId: number;
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  image: String;
+  active: boolean;
   quantity: number = 1;
 }
